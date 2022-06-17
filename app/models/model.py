96eb881ba45ffb3b2861369c17fd1416497b913b
HanzhangLi <840497622@qq.com>
2022-06-17 15:02:23 +0800

    car表改为user表

--- app/models/model.py
+++ app/models/model.py
@@ -1,10 +1,16 @@
 from app.dbs import db
 
 
-class Car(db.Model):
-    __tablename__ = "car"
-    car_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+class User(db.Model):
+    __tablename__ = "user"
+    user_id = db.Column(db.String(30), primary_key=True, nullable=False)
+    car_id = db.Column(db.Integer, autoincrement=True)
     car_type = db.Column(db.String(10), mullable=False)
-    user_id = db.Column(db.String(20), nullable=False)
 
 
+# class Wholefault(db.Model):
+#     __tablename__ = "wholefault"
+class SystemFault(db.Model):
+    __tablename__ = "system_fault"
+    fault_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    fault_name = db.Column(db.String(30), nullable=False)
