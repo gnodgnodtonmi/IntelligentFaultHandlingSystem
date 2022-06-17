72b62f6864057c14ef39f0a1ce9ad6af299beb26
HanzhangLi <840497622@qq.com>
2022-06-17 16:02:43 +0800

    故障表

--- app/models/model.py
+++ app/models/model.py
@@ -1,6 +1,7 @@
 from app.dbs import db
 
 
+# 车主与车辆信息
 class User(db.Model):
     __tablename__ = "user"
     user_id = db.Column(db.String(30), primary_key=True, nullable=False)
@@ -8,9 +9,25 @@ class User(db.Model):
     car_type = db.Column(db.String(10), mullable=False)
 
 
-# class Wholefault(db.Model):
-#     __tablename__ = "wholefault"
+# 将故障分为三级
 class SystemFault(db.Model):
     __tablename__ = "system_fault"
     fault_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     fault_name = db.Column(db.String(30), nullable=False)
+    solution = db.Column(db.Integer, db.ForeignKey("solution.solution_id"), nullable=True)
+
+
+class StructureFault(db.Model):
+    __tablename__ = "structure_fault"
+    fault_id = db.Column(db.Integer, primary_key=True)
+    fault_name = db.Column(db.String(30), nullable=False)
+    parent_fault = db.Column(db.Integer, db.ForeignKey("system_fault.fault_id"))
+    solution = db.Column(db.Integer, db.ForeignKey("solution.solution_id"), nullable=True)
+
+
+class CellFault(db.Model):
+    __tablename__ = "cell_fault"
+    fault_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    fault_name = db.Column(db.String(30), nullable=False)
+    parent_fault = db.Column(db.Integer, db.ForeignKey("structure_fault.fault_id"))
+    solution = db.Column(db.Integer, db.ForeignKey("solution.solution_id"), nullable=True)
