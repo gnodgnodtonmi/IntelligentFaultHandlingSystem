419b556d4199f454dd746ee9ea695e810261c2a5
Lu_ZhengDong <2910641515@qq.com>
2022-06-18 21:40:50 +0800

    增加了按照结构故障Id查找单元故障列表接口

--- app/views/fault/self_service_view.py
+++ app/views/fault/self_service_view.py
@@ -1,7 +1,7 @@
 from flask import jsonify, request
 
 from . import fault_bp
-from app.models.model import SystemFault, StructureFault
+from app.models.model import SystemFault, StructureFault, CellFault
 
 
 @fault_bp.route('/getAllSystemFaults', methods=['GET'])
@@ -23,10 +23,14 @@ def get_all_system_faults():
 def get_structure_fault_by_id():
     system_fault_id = request.args.get('faultId')
     if system_fault_id is None:
-        return jsonify({'msg': "missing 1 required argument \'fault_id\'"})
+        return jsonify({'msg': "missing 1 required argument \'faultId\'"})
 
-    system_faults = SystemFault.query.filter_by(fault_id=system_fault_id).all()[0]
-    system_fault_name = system_faults.fault_name
+    system_faults = SystemFault.query.filter_by(fault_id=system_fault_id).all()
+    if len(system_faults) == 0:
+        return jsonify({'msg': f'Can not found the SystemFault(faultId={system_fault_id})'})
+    system_fault = system_faults[0]
+
+    system_fault_name = system_fault.fault_name
     struct_faults = StructureFault.query.filter_by(parent_fault=system_fault_id).all()
     res = {'msg': 'success',
            'system_fault_name': system_fault_name,
@@ -38,5 +42,32 @@ def get_structure_fault_by_id():
         data = {'fault_id': struct_fault_id,
                 'fault_name': struct_fault_name}
         res['struct_fault_list'].append(data)
+
+    return jsonify(res)
+
+
+@fault_bp.route('/getCellFaultById', methods=['GET'])
+def get_cell_fault_by_id():
+    struct_fault_id = request.args.get('faultId')
+    if struct_fault_id is None:
+        return jsonify({'msg': "missing 1 required argument \'faultId\'"})
+
+    struct_faults = StructureFault.query.filter_by(fault_id=struct_fault_id).all()
+    if len(struct_faults) == 0:
+        return jsonify({'msg': f'Can not found the SystemFault(faultId={struct_fault_id})'})
+    struct_fault = struct_faults[0]
+
+    struct_fault_name = struct_fault.fault_name
+    cell_faults = CellFault.query.filter_by(parent_fault=struct_fault_id).all()
+    res = {'msg': 'success',
+           'struct_fault_name': struct_fault_name,
+           'cell_fault_list': []}
+
+    for cell_fault in cell_faults:
+        cell_fault_id = cell_fault.fault_id
+        cell_fault_name = cell_fault.fault_name
+        data = {'fault_id': cell_fault_id,
+                'fault_name': cell_fault_name}
+        res['cell_fault_list'].append(data)
 
     return jsonify(res)
