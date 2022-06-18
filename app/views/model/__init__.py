e5b73c4d97be048b3e181c7cc8c00d7d0e43de80
Lu_ZhengDong <2910641515@qq.com>
2022-06-19 00:16:26 +0800

    增加了部分导入模型的操作

--- app/views/model/__init__.py
+++ app/views/model/__init__.py
@@ -1,7 +1,9 @@
 from flask import Blueprint
 from paddlenlp import Taskflow
+from sentence_transformers import SentenceTransformer
 
 model_bp = Blueprint("model", __name__, url_prefix='/model')
-ie = Taskflow('information_extraction', schema={'故障触发词': ['部位', '形容词']})
+uie_model = Taskflow('information_extraction', schema={'故障触发词': ['部位', '形容词']})
+sbert_model = SentenceTransformer("uer/sbert-base-chinese-nli")
 
 from . import model_view
