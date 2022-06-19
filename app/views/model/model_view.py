2c37eb37b6c70bc8a9aed018459eeb738f532ffc
Lu_ZhengDong <2910641515@qq.com>
2022-06-19 22:35:27 +0800

    修正了导入模型的操作

--- app/views/model/model_view.py
+++ app/views/model/model_view.py
@@ -1,12 +1,12 @@
 from flask import jsonify, request
 
-from . import model_bp, ie
+from . import model_bp, uie_model
 
 
 @model_bp.route('/extractFaultEvent')
 def extract_fault_event():
     sentence = request.args.get('sentence')
-    res = ie([sentence])
+    res = uie_model([sentence])
     res = res[0]
     res['sentence'] = sentence
 
