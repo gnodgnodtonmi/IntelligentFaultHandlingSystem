105bc9cea4464f5cda55ec92a020d6e96faddfa6
Lu_ZhengDong <2910641515@qq.com>
2022-06-20 11:04:21 +0800

    增加了模型文本语义匹配的工具方法

--- app/utils/model_utils.py
+++ app/utils/model_utils.py
@@ -1,4 +1,7 @@
-from app.views.model import uie_model
+import torch
+from torch import cosine_similarity
+
+from app.views.model import uie_model, sbert_model
 
 
 def extract_fault_event(sentence: str) -> dict:
@@ -8,7 +11,28 @@ def extract_fault_event(sentence: str) -> dict:
     if len(fault_event.items()) == 0:
         return res
     res['fault_trigger'] = fault_event['故障触发词']
-    res['fault_location'] = fault_event['部位'] if '部位' in fault_event.keys() else None
-    res['fault_adjective'] = fault_event['形容词'] if '形容词' in fault_event.keys() else None
+    res['fault_location'] = fault_event['故障触发词']['部位'] \
+        if '部位' in fault_event['故障触发词'].keys() else None
+    res['fault_adjective'] = fault_event['故障触发词']['形容词'] \
+        if '形容词' in fault_event['故障触发词'].keys() else None
+
+    return res
+
+
+def text_semantic_matching(key_sentence: str,
+                           query_sentence_list
+                           ) -> dict:
+    key_encode = sbert_model.encode(key_sentence)
+    query_encode = sbert_model.encode(query_sentence_list)
+
+    key_tensor = torch.tensor(key_encode)
+    query_tensor = torch.tensor(query_encode)
+
+    cos_sim = cosine_similarity(key_tensor, query_tensor)
+    match_index = cos_sim.argmax()
+    match_text = query_sentence_list[match_index]
+
+    res = {'match_index': match_index,
+           'match_text': match_text}
 
     return res
--- app/views/model/model_view.py
+++ app/views/model/model_view.py
@@ -4,10 +4,15 @@ from . import model_bp
 from app.utils.model_utils import extract_fault_event
 
 
-@model_bp.route('/extractFault')
+@model_bp.route('/extractFault', methods=['GET'])
 def extract_fault():
     sentence = request.args.get('sentence')
     res = extract_fault_event(sentence=sentence)
     res['sentence'] = sentence
 
     return jsonify(res)
+
+
+@model_bp.route('/AutoLocateFault')
+def AutoLocateFault():
+    sentence = request.args.get('sentence')
